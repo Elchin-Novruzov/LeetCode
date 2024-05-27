e9bab513f59bf68b23b46ad1fce04f75f58a9fa9
Elchin Novruzov <88887189+Elchin-Novruzov@users.noreply.github.com>
2024-05-27 04:09:50 +0200

    Create 704. Binary Search.js

---
+++ 704. Binary Search.js
@@ -0,0 +1,10 @@
+var search = function(nums, target) {
+    let l = 0, r= nums.length - 1
+    while(l <= r) {
+        let m = Math.floor((r+l)/2)
+        if(nums[m] === target) return m
+        else if(nums[m] < target) l = m + 1
+        else r = m - 1
+    }
+    return -1
+};
