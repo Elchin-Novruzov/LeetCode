11316ab1d921fd872262e8259cc8232632703686
Elchin Novruzov <88887189+Elchin-Novruzov@users.noreply.github.com>
2024-07-13 03:16:33 +0200

    Create 42. Trapping Rain Water.ts

---
+++ 42. Trapping Rain Water.ts
@@ -0,0 +1,21 @@
+function trap(height: number[]): number {
+     let left = 0;
+    let right = height.length - 1;
+    let leftMax = height[left];
+    let rightMax = height[right];
+    let water = 0;
+
+    while (left < right) {
+        if (leftMax < rightMax) {
+            left++;
+            leftMax = Math.max(leftMax, height[left]);
+            water += leftMax - height[left];
+        } else {
+            right--;
+            rightMax = Math.max(rightMax, height[right]);
+            water += rightMax - height[right];
+        }
+    }
+
+    return water;    
+};
